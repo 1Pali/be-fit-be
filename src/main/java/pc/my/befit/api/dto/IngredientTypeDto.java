8672f8c0b935c184b9bf04ed7b426aea7d9795f1
Pavol Cina <pavol.cina@sap.com>
2020-08-25 15:57:50 +0200

    implementing ingredient type

---
+++ src/main/java/pc/my/befit/api/dto/IngredientTypeDto.java
@@ -0,0 +1,13 @@
+package pc.my.befit.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngredientTypeDto {
+    private Integer id;
+    private String name;
+}
