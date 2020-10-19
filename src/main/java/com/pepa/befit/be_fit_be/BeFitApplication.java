88bb17a47e95362534f307fa7afc6d55988e8a7b
Peter Kováč <peter.kovac@sap.com>
2020-10-19 18:11:57 +0200

    heroku env vars printed in console

--- src/main/java/com/pepa/befit/be_fit_be/BeFitApplication.java
+++ src/main/java/com/pepa/befit/be_fit_be/BeFitApplication.java
@@ -1,7 +1,9 @@
 package com.pepa.befit.be_fit_be;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BeFitApplication {
@@ -10,4 +12,17 @@ public class BeFitApplication {
 		SpringApplication.run(BeFitApplication.class, args);
 	}
 
+	@Component
+	public class CommandLineAppStartupRunner implements CommandLineRunner {
+		public CommandLineAppStartupRunner() {
+			
+		}
+
+		@Override
+		public void run(String...args) throws Exception {
+			System.out.println(System.getenv("SPRING_DATASOURCE_URL"));
+			System.out.println(System.getenv("SPRING_DATASOURCE_USERNAME"));
+			System.out.println(System.getenv("SPRING_DATASOURCE_PASSWORD"));
+		}
+	}
 }
